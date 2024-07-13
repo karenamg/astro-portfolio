59c631d0ffb5611f3e73b454277f86e329274308
karenamg <kamoran.19@est.ucab.edu.ve>
2024-07-13 12:28:40 -0400

    Add Contact for mobile section

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  darkMode: "class",
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        light: "#FFFFFF",
+        dark: "#1A202C",
+        darkOne: "#171923",
+        mandatory: "#FC8181",
+        lightDark: "#767676",
+        blue: "#4299E1",
+        purpleLight: "#d6bcfa",
+      },
+    },
+  },
+  plugins: [],
+};
